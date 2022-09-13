1b93aeea1099399d93ad2313cfa9a95d09c65a13
Kenzohfs <kenzo_sato@estudante.sc.senai.br>
2022-09-13 11:30:17 -0300

    activity 2 upload

--- JS05-02_kenzoSato/script.js
+++ JS05-02_kenzoSato/script.js
@@ -4,4 +4,19 @@ function cadastrar() {
     let peso = document.getElementById("peso").value;
     let alimentado = document.getElementById("alimentado").value;
     let resfriado = document.getElementById("resfriado").value;
+
+
+    if (!(nome && idade && peso)) {
+        alert("Insira todas os campos para poder se cadastrar!");
+    } else {
+        if (pessoaValida(idade, peso, alimentado, resfriado)) {
+            alert("Olá " + nome + ", você está apto a se cadastrar!");
+        } else {
+            alert("Olá " + nome + ", infelizmente você não está apto a se cadastrar.");
+        }
+    }
+}
+
+function pessoaValida(idade, peso, alimentado, resfriado) {
+    return idade >= 16 && idade <= 69 && peso >= 50 && alimentado == "sim" && resfriado == "nao";
 }
